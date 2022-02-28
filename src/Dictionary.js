a6b92619971d705d0ac8d997185830dc4116dbec
tranquynhanh <tranquynhanh902911@gmail.com>
2022-02-28 22:28:56 +0700

    search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import "./Dictionary"
+
+export default function Dictionary() {
+    let [keyWord, setKeyWord] = useState("")
+    function search(e) {
+        e.preventDefault();
+    }
+
+    function handleKeyWordChange(e) {
+        e.preventDefault();
+        setKeyWord(e.target.value());
+    }
+
+    return (
+        <div className="dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeyWordChange}/> 
+            </form>
+        </div>
+    )
+}
